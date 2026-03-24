57d1175fa6d18eacf79d013bee8404d4875f522b
Danika Paulidor <danikapaulidor@gmail.com>
2026-03-24 16:32:36 -0400

    Ajout de useState pour gérer les employés

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import { useState } from "react";
+import EmployeeList from "./components/EmployeeList";
+import EmployeeForm from "./components/EmployeeForm";
 
 function App() {
-  
+  const [employees, setEmployees] = useState([
+    { name: "John Doe", position: "Manager", department: "HR", status: "Actif" },
+  ]);
+
+  const addEmployee = (employee) => {
+    setEmployees([...employees, { ...employee, status: "Actif" }]);
+  };
 
   return (
-    <>
-     
-    </>
-  )
+    <div>
+      <EmployeeForm addEmployee={addEmployee} />
+      <EmployeeList employees={employees} />
+    </div>
+  );
 }
 
-export default App
+export default App;
